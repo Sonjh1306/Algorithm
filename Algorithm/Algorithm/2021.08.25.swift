b52db491fa54153b4385ad5b9e940e43bbd8f29c
Sonjh1306 <yale1306@gmail.com>
2021-08-25 17:57:28 +0900

    프로그래머스: 직업군 추천하기

---
+++ Algorithm/Algorithm/2021.08.25.swift
@@ -0,0 +1,33 @@
+import Foundation
+
+class RecommendOccupationGroups {
+    func solution(_ table:[String], _ languages:[String], _ preference:[Int]) -> String {
+        var dict: [String: Int] = ["SI": 0, "CONTENTS": 0, "HARDWARE": 0, "PORTAL": 0, "GAME": 0]
+        var si = table[0].components(separatedBy: " ")
+        var contents = table[1].components(separatedBy: " ")
+        var hardware = table[2].components(separatedBy: " ")
+        var portal = table[3].components(separatedBy: " ")
+        var game = table[4].components(separatedBy: " ")
+        
+        si.reverse()
+        contents.reverse()
+        hardware.reverse()
+        portal.reverse()
+        game.reverse()
+        
+        for i in 0..<languages.count {
+            let siIndex = si.firstIndex(of: languages[i]).map {Int($0)} ?? 0
+            let contentsIndex = contents.firstIndex(of: languages[i]).map {Int($0)} ?? 0
+            let hardwareIndex = hardware.firstIndex(of: languages[i]).map {Int($0)} ?? 0
+            let portalIndex = portal.firstIndex(of: languages[i]).map {Int($0)} ?? 0
+            let gameIndex = game.firstIndex(of: languages[i]).map {Int($0)} ?? 0
+            dict["SI"]! += ((siIndex != 0 ? siIndex + 1 : siIndex) * preference[i])
+            dict["CONTENTS"]! += ((contentsIndex != 0 ? contentsIndex + 1 : contentsIndex) * preference[i])
+            dict["HARDWARE"]! += ((hardwareIndex != 0 ? hardwareIndex + 1 : hardwareIndex) * preference[i])
+            dict["PORTAL"]! += ((portalIndex != 0 ? portalIndex + 1 : portalIndex) * preference[i])
+            dict["GAME"]! += ((gameIndex != 0 ? gameIndex + 1 : gameIndex) * preference[i])
+        }
+        return dict.sorted(by: { $0.value == $1.value ? $0.key < $1.key : $0.value > $1.value}).first!.key
+    }
+}
+
--- Algorithm/Algorithm/main.swift
+++ Algorithm/Algorithm/main.swift
@@ -1,20 +1,8 @@
 import Foundation
 
-let rabbitsInForest = RabbitsInForest()
-//print(rabbitsInForest.numRabbits([10,10,10]))
-//print(rabbitsInForest.numRabbits([1,1,2]))
-print(rabbitsInForest.numRabbits([1,0,1,0,0]))
-//print(rabbitsInForest.numRabbits([0,0,1,1,1]))
+let recommendOccupationGroups = RecommendOccupationGroups()
+print(recommendOccupationGroups.solution(["SI JAVA JAVASCRIPT SQL PYTHON C#", "CONTENTS JAVASCRIPT JAVA PYTHON SQL C++", "HARDWARE C C++ PYTHON JAVA JAVASCRIPT", "PORTAL JAVA JAVASCRIPT PYTHON KOTLIN PHP", "GAME C++ C# JAVASCRIPT C JAVA"], ["PYTHON", "C++", "SQL"], [7, 5, 5]))
 
-//let jewelsAndStones = JewelsAndStones()
-//print(jewelsAndStones.numJewelsInStones("aA", "aAAbbbb"))
-//print(jewelsAndStones.numJewelsInStones("z", "ZZ"))
-//
-//let sortingTheSentence = SortingTheSentence()
-//print(sortingTheSentence.sortSentence("is2 sentence4 This1 a3"))
-//print(sortingTheSentence.sortSentence("Myself2 Me1 I4 and3"))
-
-
-
+print(recommendOccupationGroups.solution(["SI JAVA JAVASCRIPT SQL PYTHON C#", "CONTENTS JAVASCRIPT JAVA PYTHON SQL C++", "HARDWARE C C++ PYTHON JAVA JAVASCRIPT", "PORTAL JAVA JAVASCRIPT PYTHON KOTLIN PHP", "GAME C++ C# JAVASCRIPT C JAVA"], ["JAVA", "JAVASCRIPT"], [7, 5]))
 
 
