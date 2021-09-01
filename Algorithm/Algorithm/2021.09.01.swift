6f154b9c788741a9c06753846988f613fe5afb2c
Sonjh1306 <yale1306@gmail.com>
2021-09-02 00:35:04 +0900

    LeetCode: Binary Search(704)

--- Algorithm/Algorithm/2021.09.01.swift
+++ Algorithm/Algorithm/2021.09.01.swift
@@ -9,3 +9,14 @@ class RotateArray {
     }
 }
 
+
+class BinarySearch {
+    func search(_ nums: [Int], _ target: Int) -> Int {
+        for i in 0..<nums.count {
+            if nums[i] == target {
+                return i
+            }
+        }
+        return -1
+    }
+}
--- Algorithm/Algorithm/main.swift
+++ Algorithm/Algorithm/main.swift
@@ -1,5 +1,10 @@
 import Foundation
 
+let rotateArray = RotateArray()
+var nums = [1,2,3,4]
+rotateArray.rotate(&nums, 3)
 
 
-
+let binarySearch = BinarySearch()
+print(binarySearch.search([-1,0,3,5,9,12], 9))
+print(binarySearch.search([-1,0,3,5,9,12], 2))
