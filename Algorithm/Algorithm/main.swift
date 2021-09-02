3a85c3221157253f942017085d36ee23cba6782f
Sonjh1306 <yale1306@gmail.com>
2021-09-02 20:09:33 +0900

    LeetCode: Reverse Words in a String III(557)

--- Algorithm/Algorithm/main.swift
+++ Algorithm/Algorithm/main.swift
@@ -1,6 +1,4 @@
 import Foundation
 
-let twoSumII = TwoSumII()
-//print(twoSumII.twoSum([2,7,11,15], 9))
-print(twoSumII.twoSum([2,3,4], 6))
-//print(twoSumII.twoSum([-1,0], -1))
+let reverseWordsInAStringIII = ReverseWordsInAStringIII()
+print(reverseWordsInAStringIII.reverseWords("Let's take LeetCode contest"))
