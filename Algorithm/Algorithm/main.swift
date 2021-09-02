2ea00524c2b3972f63435a19864ec8d6ebc7e315
Sonjh1306 <yale1306@gmail.com>
2021-09-02 16:14:24 +0900

    LeetCode: Squares of a Sorted Array(977)

--- Algorithm/Algorithm/main.swift
+++ Algorithm/Algorithm/main.swift
@@ -1,8 +1,12 @@
 import Foundation
 
-let searchInsertPosition = SearchInsertPosition()
-print(searchInsertPosition.searchInsert([1,3,5,6], 5))
-print(searchInsertPosition.searchInsert([1,3,5,6], 2))
-print(searchInsertPosition.searchInsert([1,3,5,6], 7))
-print(searchInsertPosition.searchInsert([1,3,5,6], 0))
-print(searchInsertPosition.searchInsert([1], 0))
+//let searchInsertPosition = SearchInsertPosition()
+//print(searchInsertPosition.searchInsert([1,3,5,6], 5))
+//print(searchInsertPosition.searchInsert([1,3,5,6], 2))
+//print(searchInsertPosition.searchInsert([1,3,5,6], 7))
+//print(searchInsertPosition.searchInsert([1,3,5,6], 0))
+//print(searchInsertPosition.searchInsert([1], 0))
+
+let squaresOfASortedArray = SquaresOfASortedArray()
+print(squaresOfASortedArray.sortedSquares([-4,-1,0,3,10]))
+print(squaresOfASortedArray.sortedSquares([-7,-3,2,3,11]))
