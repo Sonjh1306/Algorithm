d5d3c55c9cc26f3022af581c9ef566bdce6309c5
Sonjh1306 <yale1306@gmail.com>
2021-09-02 18:33:49 +0900

    LeetCode:Two Sum II - Input array is sorted(167)

--- Algorithm/Algorithm/main.swift
+++ Algorithm/Algorithm/main.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-let moveZeroes = MoveZeroes()
-var nums = [0,1,0,3,12]
-moveZeroes.moveZeroes(&nums)
-print(nums)
+let twoSumII = TwoSumII()
+print(twoSumII.twoSum([2,7,11,15], 9))
+print(twoSumII.twoSum([2,3,4], 6))
+print(twoSumII.twoSum([-1,0], -1))
