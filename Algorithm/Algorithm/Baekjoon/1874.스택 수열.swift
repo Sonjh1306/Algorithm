0dff4961d2d9c78679afb4828fe2230fdd6f198d
Sonjh1306 <yale1306@gmail.com>
2021-11-13 19:34:11 +0900

    백준: 스택수열(1847)

--- Algorithm/Algorithm/Baekjoon/1874.스택 수열.swift
+++ Algorithm/Algorithm/Baekjoon/1874.스택 수열.swift
@@ -1,3 +1,30 @@
 import Foundation
 
+class StackSequence {
+    func solution() {
+        let n = Int(readLine()!)!
 
+        var count = 1
+        var stack: [Int] = []
+        var answer: [String] = []
+
+        for _ in 0..<n {
+            let num = Int(readLine()!)!
+            
+            while count <= num {
+                stack.append(count)
+                answer.append("+")
+                count += 1
+            }
+            
+            if stack.last == num {
+                stack.removeLast()
+                answer.append("-")
+            } else {
+                print("NO")
+                exit(0)
+            }
+        }
+        print(answer.joined(separator: "\n"))
+    }
+}
--- Algorithm/Algorithm/main.swift
+++ Algorithm/Algorithm/main.swift
@@ -1,5 +1,4 @@
 import Foundation
 
-let printer = Printer()
-print(printer.solution([2, 1, 3, 2], 2))
-print(printer.solution([1, 1, 9, 1, 1, 1], 0))
+let stackSequence = StackSequence()
+stackSequence.solution()
